2d078e4cd2b6c816994b52da8b11c1e12f2aa2bd
leduong6789 <143622093+leduong6789@users.noreply.github.com>
2023-11-18 22:00:28 +0700

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,15 +1,72 @@
 #include <stdio.h>
 
-int main()
-{
-    for(int i=1;i<=10;i++){
-        for(int j=2;j<=9;j++){
-            printf("%c%2dx%2d=%3d",179,j,i,i*j);
-        }
-        printf("%c\n",179);
-    }
-
-    return 0;
+void Nhap(int *ts, int *ms){
+    printf("Nhap tu so: ");
+    scanf("%d", ts);
+    do{
+        printf("Nhap mau so: ");
+        scanf("%d", ms);
+    }while(*ms == 0);
 }
 
+void Tong(int ts1,int ms1,int ts2,int ms2,int *tskq,int *mskq){
+    *tskq = ts1*ms2 + ts2*ms1;
+    *mskq = ms1*ms2;
+}
+void Hieu(int ts1,int ms1,int ts2,int ms2,int *tskq,int *mskq){
+	*tskq = ts1*ms2 - ts2*ms1;
+    *mskq = ms1*ms2;
+}
+void Tich(int ts1,int ms1,int ts2,int ms2,int *tskq,int *mskq){
+	*tskq = ts1*ts2;
+	*mskq = ms1*ms2;
+}
+void Thuong(int ts1,int ms1,int ts2,int ms2,int *tskq,int *mskq,int *error){
+	if(ts2==0)
+		*error = 1;
+	else{
+		*tskq = ts1*ms2;
+		*mskq = ms1*ts2;
+	}
+}
+char ChonPhepTinh(){
+	char pt;
+	do{
+		printf("Moi nhap phep tinh (+,-,*,/): ");
+		scanf(" %c",&pt);
+	}while(pt!='+'&&pt!='-'&&pt!='*'&&pt!='/');
+	return pt;
+}
+void NhapCacPhanSo(int ts1, int ms1, int ts2, int ms2, int *tskq, int *mskq, int *error){
+	char phepToan=ChonPhepTinh();
+	switch(phepToan){
+		case '+':
+			Tong(ts1,ms1,ts2,ms2,tskq,mskq);
+			printf("%d / %d",*tskq,*mskq);
+			break;
+		case '-':
+			Hieu(ts1, ms1, ts2,ms2,tskq,mskq);
+			printf("%d / %d",*tskq,*mskq);
+			break;
+		case '*':
+			Tich(ts1, ms1, ts2,ms2,tskq,mskq);
+			printf("%d / %d",*tskq,*mskq);
+			break;
+		default:
+			Thuong(ts1, ms1, ts2,ms2,tskq,mskq,error);
+			if(*error == 1){
+				printf("Error: Division by zero is not allowed.\n");
+			}else{
+				printf("%d / %d",*tskq,*mskq);
+			}
+	}
+}
+int main(){
+    int ts1, ms1, ts2, ms2, tskq, mskq,error;
+    printf("Nhap phan so thu nhat:\n");
+    Nhap(&ts1, &ms1);
+    printf("Nhap phan so thu hai:\n");
+    Nhap(&ts2, &ms2);
+	NhapCacPhanSo(ts1,ms1,ts2,ms2,&tskq,&mskq,&error);
+}
 
