acd3a6b802b56af09cf4b1a6c905669aea57e078
leduong6789 <143622093+leduong6789@users.noreply.github.com>
2024-04-05 08:28:47 +0700

    Update Bai 1 tuan 10.c

--- Bai 1 tuan 10.c
+++ Bai 1 tuan 10.c
@@ -1,119 +1,68 @@
-// sieu so nguyen to 
 #include <stdio.h>
-#include <math.h>
-int isprime(int n,int i){
-    if(n<=2) return n==2;
-    if(n % i==0) return 0;
-    if(i*i > n) return 1;
-    return isprime(n,i+1);
+
+int isPrime(int n, int i) {
+    if (n <= 2)
+        return n == 2;
+    if (n % i == 0)
+        return 0;
+    if (i * i > n)
+        return 1;
+    return isPrime(n, i + 1);
 }
-int isSuperPrime(int n){
-    if(!isprime(n,2)) return 0;
-    if(n<10) return 1;
-    return isSuperPrime(n/10);
+
+int SieuNguyenTo(int n) {
+    if (!isPrime(n, 2))
+        return 0;
+    if (n < 10)
+        return isPrime(n, 2);
+    return isPrime(n, 2) && SieuNguyenTo(n / 10);
 }
-// so emirp
-int sodao(int n,int sdn){
-    if(n==0) return sdn;
-    else{
-        sdn = sdn*10 + n%10;
-        return sodao(n/10,sdn);
+int CongSo(int n,int s){
+	if(n==0) return s;
+	else{
+		s = s*10 + n%10;
+		CongSo(n/10,s);
+	}
+}
+int PerPrime(int n){
+	if(!isPrime(n,2)) return 0;
+	if(n < 10) return isPrime(n,2);
+	int m = CongSo(n,0);
+	return isPrime(m,2) && PerPrime(m);
+	
+}
+int Max(int a[], int n) {
+    if (n == 1)
+        return SieuNguyenTo(a[0]) ? a[0] : -1;
+    else {
+        int x = Max(a, n - 1);
+        if (SieuNguyenTo(a[n - 1]) && (a[n - 1] > x || x == -1))
+            return a[n - 1];
+        else
+            return x;
     }
 }
-int PalindromePrime(int n){
-    if(!isprime(n,2)) return 0;
-    int kq = sodao(n,0);
-    return kq == n;
-}
-int isEmirp(int n){
-    if(!isprime(n,2)) return 0;
-    int kq = sodao(n,0);
-    return kq != n && isprime(kq,2);
-}
-int findMaxSoSieuNguyenTo(int A[],int n){
-    if(n==0) return -1;
-    if(n==1) return isSuperPrime(A[0]) ? A[0] : 0;
-    else{
-        int kq = findMaxSoSieuNguyenTo(A,n-1);
-        if(A[n-1] < kq && isSuperPrime(A[n-1])) return A[n-1];
-        return kq;
-    }
-}
-int findmaxSoEmirp(int A[],int n){
-    if(n==0) return -1;
-    if(n==1) return isEmirp(A[0]) ? A[0] : 0;
-    else{
-        int kq = findmaxSoEmirp(A,n-1);
-        if(A[n-1] < kq && isEmirp(A[n-1])) return A[n-1];
-        return kq;
-    }
-}
-typedef struct {
-    float x;
-    float y;
-} Point;
-
-float calculateDistance(Point A, Point B) {
-    return sqrt(pow(B.x - A.x, 2) + pow(B.y - A.y, 2));
-}
-
-float findMinDistance(Point points[], int numPoints) {
-    float minDistance = calculateDistance(points[0], points[1]);
-
-    for (int i = 0; i < numPoints - 1; ++i) {
-        for (int j = i + 1; j < numPoints; ++j) {
-            float distance = calculateDistance(points[i], points[j]);
-            if (distance < minDistance) {
-                minDistance = distance;
-            }
-        }
+float avgSuperPrimeHelper(int a[], int n, int index, int count, int sum) {
+    if (index == n) {
+        if (count == 0) return 0; // To avoid division by zero
+        return (float) sum / count;
     }
 
-    return minDistance;
-}
-
-float findMaxDistance(Point points[], int numPoints) {
-    float maxDistance = calculateDistance(points[0], points[1]);
-
-    for (int i = 0; i < numPoints - 1; ++i) {
-        for (int j = i + 1; j < numPoints; ++j) {
-            float distance = calculateDistance(points[i], points[j]);
-            if (distance > maxDistance) {
-                maxDistance = distance;
-            }
-        }
+    if (SieuNguyenTo(a[index])) {
+        count++;
+        sum += a[index];
     }
 
-    return maxDistance;
+    return avgSuperPrimeHelper(a, n, index + 1, count, sum);
 }
-float calculateVolume(float diameter, float height) {
-    float radius = diameter / 2.0;
-    return (1.0 / 3.0) * M_PI * pow(radius, 2) * height;
+
+float avgSuperPrime(int a[], int n) {
+    return avgSuperPrimeHelper(a, n, 0, 0, 0);
 }
 int main() {
-    Point A[] = {{1.0, 2.0}, {3.0, 4.0}, {5.0, 6.0}, {7.0, 8.0}, {9.0, 10.0}};
-    int numPoints = 5;
-
-    float minDistance = findMinDistance(A, numPoints);
-    printf("Minimum distance between points: %.2f\n", minDistance);
-
-    float maxDistance = findMaxDistance(A, numPoints);
-    printf("Maximum distance between points: %.2f\n", maxDistance);
-    // Assume height of the cone is equal to the maximum distance
-    float height = maxDistance;
-
-    // Calculate volume of the cone
-    float volume = calculateVolume(maxDistance, height);
-    printf("Volume of the cone: %.2f\n", volume);
-    // int num;
-    // int A[5] = { 79, 97, 107,17, 31  };
-    // printf("Nhap so can kiem tra: ");
-    // scanf("%d", &num);
-    // if (isEmirp(num))
-    //     printf("%d la so emirp.\n", num);
-    // else
-    //     printf("%d khong phai la so emirp.\n", num);
-    // int x = findmaxSoEmirp(A,5);
-    // printf("%d",x);
+    int a[5] = {6,733,2399,2939,3119 };
+    float x = avgSuperPrime(a, 5);
+    printf("%.2f", x);
     return 0;
 }
+
